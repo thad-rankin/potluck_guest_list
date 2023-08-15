8bf6bf7928008f42bbc4d426a5a4e8ad270802d1
Thad R <122906384+thad-rankin@users.noreply.github.com>
2023-08-15 13:59:03 -0500

    Updated JS file to include assignin dishes to attendees functionality

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+// appears when the guest list is full
+const assignButton = document.querySelector(".assign");
+// targets the list that will populate with the guest's name and their assigned dish
+const assignedItems = document.querySelector(".assigned-items");
 
 addGuestButton.addEventListener("click", function () {
     const guest = guestInput.value;
@@ -40,3 +44,35 @@ const updateGuestCount = function() {
         guestFull.classList.remove("hide");
     }
 };
+
+const assignItems = function() {
+    const potluckItems = [
+        "potato salad", 
+        "hummus", 
+        "cookies", 
+        "fruit", 
+        "bruschetta", 
+        "deviled eggs", 
+        "caprese skewers", 
+        "pasta salad", 
+        "cucumber salad", 
+        "cheesecake", 
+        "cheese tray", 
+        "chips and salsa"
+    ];
+
+    const allGuests = document.querySelectorAll(".guest-list li");
+    for (let guest of allGuests) {
+        let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+        let randomPotluckItem = potluckItems[randomPotluckIndex];
+        let listItem = document.createElement("li");
+        listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+        assignedItems.append(listItem);
+        potluckItems.splice(randomPotluckIndex, 1);
+    }
+}
+
+assignButton.addEventListener("click", function () {
+    assignItems();
+    assignButton.disabled = true;
+});
